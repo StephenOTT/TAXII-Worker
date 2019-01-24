ba32a3aa0c0c1a1f494a62baec9ed829037e3517
StephenOTT <stephen@digitalstate.ca>
2019-01-24 12:16:12 -0500

    rename jsonconfig class

--- src/main/java/io/digitalstate/taxii/camunda/worker/common/VertxObjectMapperConfig.java
+++ src/main/java/io/digitalstate/taxii/camunda/worker/common/VertxObjectMapperConfig.java
@@ -1,11 +1,11 @@
-package io.digitalstate.taxii.camunda.worker;
+package io.digitalstate.taxii.camunda.worker.common;
 
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import io.vertx.core.json.Json;
 
-public class JsonConfig {
+public class VertxObjectMapperConfig {
 
     public static void registerModules(){
         Json.mapper.registerModule(new ParameterNamesModule())
